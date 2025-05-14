dadf18ffeefbd2b0fdad0611eddbc85781c11204
juliacevedo <juliacevedo.psi@gmail.com>
2025-05-14 01:05:49 -0300

    subido

---
+++ src/ar/edu/ungs/prog2/ticketek/Entrada.java
@@ -0,0 +1,17 @@
+package ar.edu.ungs.prog2.ticketek;
+
+public class Entrada implements IEntrada {
+
+	@Override
+	public double precio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String ubicacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
